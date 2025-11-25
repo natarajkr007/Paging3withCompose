25491c7e7cf05350e2cbd264c21928fcd5ede458
Nataraj KR <natarajkr007@gmail.com>
2025-11-25 16:05:10 +0530

    1. Paddings adjusted to make the edge to edge look and feel much better.

--- app/src/main/java/com/nataraj/paging3/MainActivity.kt
+++ app/src/main/java/com/nataraj/paging3/MainActivity.kt
@@ -6,9 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
@@ -22,6 +26,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
@@ -44,13 +51,22 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Box(
                         modifier = Modifier
-                            .padding(innerPadding)
+                            .padding(
+                                innerPadding.calculateStartPadding(LocalLayoutDirection.current),
+                                0.dp,
+                                innerPadding.calculateEndPadding(LocalLayoutDirection.current),
+                                0.dp
+                            )
                             .fillMaxSize()
                     ) {
                         LazyColumn {
                             items(
                                 count = dummyProductsPagingData.itemCount
                             ) { index ->
+                                if (index == 0) {
+                                    Spacer(modifier = Modifier.size(innerPadding.calculateTopPadding()))
+                                }
+
                                 UserPagingListItem(
                                     dummyProductsPagingData[index]?.title ?: "-",
                                     modifier = Modifier
@@ -60,13 +76,21 @@ class MainActivity : ComponentActivity() {
                             }
 
                             item {
-                                if (dummyProductsPagingData.loadState.append is LoadState.Loading) {
-                                    CircularProgressIndicator(
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .padding(16.dp)
-                                    )
+                                Box(
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    if (dummyProductsPagingData.loadState.append is LoadState.Loading) {
+                                        CircularProgressIndicator()
+                                    } else {
+                                        Text(
+                                            stringResource(R.string.list_end_message),
+                                            color = Color.Gray
+                                        )
+                                    }
                                 }
+
+                                Spacer(modifier = Modifier.size(innerPadding.calculateBottomPadding() + 16.dp))
                             }
                         }
 
